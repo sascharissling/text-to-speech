602e022b1288e608dca503c9b2ec2ae372d417fa
Sascha Rissling <53901545+sascharissling@users.noreply.github.com>
2019-12-03 08:18:07 +0100

    add text to speech function (#2)

---
+++ src/GlobalStyles.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Global, css } from "@emotion/core";
+
+export default function GlobalStyles() {
+  return (
+    <Global
+      styles={css`
+        *,
+        *::after,
+        *::before {
+          box-sizing: border-box;
+        }
+
+        body {
+          margin: 0;
+          font-family: "Roboto Slab", serif;
+          background: #2e303d;
+        }
+      `}
+    />
+  );
+}
